ae47454032f3d4a096fe2f4a6eef0ad4690b02a1
brox27 <oystein.brox@gmail.com>
2017-01-31 12:26:11 +0100

    started on Order Manager

---
+++ Order Manager/Network.go
@@ -0,0 +1,26 @@
+package main
+import (
+//	."fmt"
+)
+
+// Variables that should come from somewhere else!!
+var NUM_ELEVATORS = 3;
+var Elevators_Alive = 3;
+// end of var list
+
+
+func Assign_Order(){
+//	var cost_table [3]int;
+	var lowest = -1;
+	var cost = 1;
+	for elev := 0; elev < Elevators_Alive; elev++{
+		println("lol");
+		cost = 12+elev;
+		if cost<=lowest
+	}
+}
+
+
+func main() {
+	println("hei...");
+}
