81b26514fa5ecc3063aa1b1bc23a5f94e93e3321
petermst <kji>
2017-03-01 16:15:54 +0100

    update consensus -> two funcs

--- New Complete/ConsensusHall.go
+++ New Complete/ConsensusHall.go
@@ -13,8 +13,6 @@ func ConsensusHall(ClearHallOrderChan chan [2]int, HallButtonChan chan [2]int, P
 	// BEM EM IN SCUTTY
 	// hallordersTx := make(chan ConfigFile.AllHallOrders)						// REMEMBER TO USE
 	// BEM EM UP SCUTTY
-	RemoteID = "123"
-	LocalID = "321"
 
 	for {
 		select {
@@ -24,92 +22,7 @@ func ConsensusHall(ClearHallOrderChan chan [2]int, HallButtonChan chan [2]int, P
 
 					local := localHallConsensus.HallOrders[floor][button]
 					remote := remoteHallConsensus.HallButtons[floor][button]
-
-					switch local.OrderState {
-
-					case ConfigFile.Default:
-						if RemoteID != LocalID {
-							switch remote.OrderState {
-							case ConfigFile.Default:
-								break
-							case ConfigFile.Inactive:
-								local.OrderState = ConfigFile.Inactive
-								local.AckdBy = local.AckdBy[:0] // destroy
-								break
-							case ConfigFile.PendingAck:
-								local.OrderState = ConfigFile.PendingAck
-								local.AckdBy = append(local.AckdBy, LocalID)
-								break
-							case ConfigFile.Active:
-								local.OrderState = ConfigFile.Active
-								local.AckdBy = append(local.AckdBy, LocalID)
-								//onActive() // usikker på hvordan dette kan oversettes til golang
-								varible := ConfigFile.OrderMsg{}
-								varible.Floor = floor
-								varible.Button = button
-								newOrderConsensus <- varible
-							}
-						}
-
-					case ConfigFile.Inactive:
-						switch remote.OrderState {
-						case ConfigFile.Inactive:
-							break // if state remains the same, do nothing
-
-						case ConfigFile.PendingAck:
-							local.OrderState = remote.OrderState
-							local.AckdBy = append(local.AckdBy, LocalID) //sett inn IP/ID
-							break
-
-						case ConfigFile.Active:
-							break // cannot skip PendingAck-state
-
-						}
-
-					case ConfigFile.PendingAck:
-						switch remote.OrderState {
-						case ConfigFile.Inactive:
-							break
-
-						case ConfigFile.PendingAck:
-							// ADD all others ACKed to local
-
-							if len(local.AckdBy) == 3 { // denne må selvsagt byttes til en "dynamisk" sak, og ikke bare sjekker antall!
-								local.OrderState = ConfigFile.Active
-								varible := ConfigFile.OrderMsg{}
-								varible.Floor = floor
-								varible.Button = button
-								newOrderConsensus <- varible
-
-							}
-							break
-						case ConfigFile.Active:
-							local.OrderState = ConfigFile.Active
-							//local.AckdBy
-							break
-
-						}
-
-					case ConfigFile.Active:
-						switch remote.OrderState {
-						case ConfigFile.Inactive:
-							local.OrderState = remote.OrderState
-							local.AckdBy = local.AckdBy[:0] // destroy
-							varible := ConfigFile.OrderMsg{}
-							varible.Floor = floor
-							varible.Button = button
-							completeOrderConsensus <- varible
-							break
-
-						case ConfigFile.PendingAck:
-							break
-
-						case ConfigFile.Active:
-							// Anders har appendet IP til ACked by her også... why? eller ser Why, men er det nødvendig?
-							break
-
-						}
-					}
+					merge(local, remote) // sette til å oppdatere local også!!!
 				}
 			}
 		default:
@@ -118,6 +31,100 @@ func ConsensusHall(ClearHallOrderChan chan [2]int, HallButtonChan chan [2]int, P
 	}
 }
 
+// --> Ander$ Code <--
+/*
 func doostuff(onActive func(), onInactive func()) {
 	f()
+}
+*/
+
+func merge(local ConfigFile.Orderstate, remote ConfigFile.Orderstate) {
+	RemoteID = "123"
+	LocalID = "321"
+
+	switch local.OrderState {
+
+	case ConfigFile.Default:
+		if RemoteID != LocalID {
+			switch remote.OrderState {
+			case ConfigFile.Default:
+				break
+			case ConfigFile.Inactive:
+				local.OrderState = ConfigFile.Inactive
+				local.AckdBy = local.AckdBy[:0] // destroy
+				break
+			case ConfigFile.PendingAck:
+				local.OrderState = ConfigFile.PendingAck
+				local.AckdBy = append(local.AckdBy, LocalID)
+				break
+			case ConfigFile.Active:
+				local.OrderState = ConfigFile.Active
+				local.AckdBy = append(local.AckdBy, LocalID)
+				//onActive() // usikker på hvordan dette kan oversettes til golang
+				varible := ConfigFile.OrderMsg{}
+				varible.Floor = floor
+				varible.Button = button
+				newOrderConsensus <- varible
+			}
+		}
+
+	case ConfigFile.Inactive:
+		switch remote.OrderState {
+		case ConfigFile.Inactive:
+			break // if state remains the same, do nothing
+
+		case ConfigFile.PendingAck:
+			local.OrderState = remote.OrderState
+			local.AckdBy = append(local.AckdBy, LocalID) //sett inn IP/ID
+			break
+
+		case ConfigFile.Active:
+			break // cannot skip PendingAck-state
+
+		}
+
+	case ConfigFile.PendingAck:
+		switch remote.OrderState {
+		case ConfigFile.Inactive:
+			break
+
+		case ConfigFile.PendingAck:
+			// ADD all others ACKed to local
+
+			if len(local.AckdBy) == 3 { // denne må selvsagt byttes til en "dynamisk" sak, og ikke bare sjekker antall!
+				local.OrderState = ConfigFile.Active
+				varible := ConfigFile.OrderMsg{}
+				varible.Floor = floor
+				varible.Button = button
+				newOrderConsensus <- varible
+
+			}
+			break
+		case ConfigFile.Active:
+			local.OrderState = ConfigFile.Active
+			//local.AckdBy
+			break
+
+		}
+
+	case ConfigFile.Active:
+		switch remote.OrderState {
+		case ConfigFile.Inactive:
+			local.OrderState = remote.OrderState
+			local.AckdBy = local.AckdBy[:0] // destroy
+			varible := ConfigFile.OrderMsg{}
+			varible.Floor = floor
+			varible.Button = button
+			completeOrderConsensus <- varible
+			break
+
+		case ConfigFile.PendingAck:
+			break
+
+		case ConfigFile.Active:
+			// appendet IP til ACked by her også
+			break
+
+		}
+	}
 }
---
+++ rot vi trolig kan skorte/ConsensusHall.go
@@ -0,0 +1,130 @@
+package ConsensusHall
+
+import (
+	"./ConfigFile"
+	"fmt"
+)
+
+var localHallConsensus ConfigFile.ConsensusHall
+
+func ConsensusHall(ClearHallOrderChan chan [2]int, HallButtonChan chan [2]int, PeerUpdateChan chan ConfigFile.PeerUpdate) {
+	// TIL FRA NETTVERK
+	hallordersRx := make(chan ConfigFile.ConsensusHall)
+	// BEM EM IN SCUTTY
+	// hallordersTx := make(chan ConfigFile.AllHallOrders)						// REMEMBER TO USE
+	// BEM EM UP SCUTTY
+	RemoteID = "123"
+	LocalID = "321"
+
+	for {
+		select {
+		case remoteHallConsensus := <-hallordersRx:
+			for floor := 0; floor < ConfigFile.Num_floors; floor++ {
+				for button := 0; button < ConfigFile.Num_buttons-1; button++ {
+
+					local := localHallConsensus.HallOrders[floor][button]
+					remote := remoteHallConsensus.HallButtons[floor][button]
+
+					switch local.OrderState {
+
+					case ConfigFile.Default:
+						if RemoteID != LocalID {
+							switch remote.OrderState {
+							case ConfigFile.Default:
+								break
+							case ConfigFile.Inactive:
+								local.OrderState = ConfigFile.Inactive
+								local.AckdBy = local.AckdBy[:0] // destroy
+								break
+							case ConfigFile.PendingAck:
+								local.OrderState = ConfigFile.PendingAck
+								local.AckdBy = append(local.AckdBy, LocalID)
+								break
+							case ConfigFile.Active:
+								local.OrderState = ConfigFile.Active
+								local.AckdBy = append(local.AckdBy, LocalID)
+								//onActive() // usikker på hvordan dette kan oversettes til golang
+								varible := ConfigFile.OrderMsg{}
+								varible.Floor = floor
+								varible.Button = button
+								newOrderConsensus <- varible
+							}
+						}
+
+					case ConfigFile.Inactive:
+						switch remote.OrderState {
+						case ConfigFile.Inactive:
+							break // if state remains the same, do nothing
+
+						case ConfigFile.PendingAck:
+							local.OrderState = remote.OrderState
+							local.AckdBy = append(local.AckdBy, LocalID) //sett inn IP/ID
+							break
+
+						case ConfigFile.Active:
+							break // cannot skip PendingAck-state
+
+						}
+
+					case ConfigFile.PendingAck:
+						switch remote.OrderState {
+						case ConfigFile.Inactive:
+							break
+
+						case ConfigFile.PendingAck:
+							// ADD all others ACKed to local
+
+							if len(local.AckdBy) == 3 { // denne må selvsagt byttes til en "dynamisk" sak, og ikke bare sjekker antall!
+								local.OrderState = ConfigFile.Active
+								varible := ConfigFile.OrderMsg{}
+								varible.Floor = floor
+								varible.Button = button
+								newOrderConsensus <- varible
+
+							}
+							break
+						case ConfigFile.Active:
+							local.OrderState = ConfigFile.Active
+							//local.AckdBy
+							break
+
+						}
+
+					case ConfigFile.Active:
+						switch remote.OrderState {
+						case ConfigFile.Inactive:
+							local.OrderState = remote.OrderState
+							local.AckdBy = local.AckdBy[:0] // destroy
+							varible := ConfigFile.OrderMsg{}
+							varible.Floor = floor
+							varible.Button = button
+							completeOrderConsensus <- varible
+							break
+
+						case ConfigFile.PendingAck:
+							break
+
+						case ConfigFile.Active:
+							// Anders har appendet IP til ACked by her også... why? eller ser Why, men er det nødvendig?
+							break
+
+						}
+					} // til og med denne!
+				}
+			}
+		default:
+			break
+		}
+	}
+}
+
+/*
+func doostuff(onActive func(), onInactive func()) {
+	f()
+}
+
+*/
+
+func merge() {
+
+}
